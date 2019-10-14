452e4226e59184589af4eb4a9e4412cfd89e689a
lolitiy <al.lolita96@gmail.com>
2019-10-14 20:54:07 +0300

    add script

--- 4.lists_tuples_cycles/4.7.py
+++ 4.lists_tuples_cycles/4.7.py
@@ -1,3 +1,11 @@
 # Программа, отбирающая из коллекции чисел только чётные, а затем считающая их сумму
 
 interesting_numbers = (3, 2, 5, 7, 14, 26, 32, 31, 37)
+i_n = list(interesting_numbers)
+b = []
+for i in i_n:
+    if i % 2 == 0:
+        b.append(i)
+b = tuple(b)
+s = sum(b)
+print(s)
