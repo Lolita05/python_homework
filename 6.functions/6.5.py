b492df8cc8408ce6f919e789f8c9a8dc53c4ec4c
lolitiy <al.lolita96@gmail.com>
2019-11-19 23:51:11 +0300

    add script

--- 6.functions/6.5.py
+++ 6.functions/6.5.py
@@ -1,5 +1,14 @@
 #Вычисление среднего
-def mean(numbers):
-    return float(sum(numbers)) / max(len(numbers), 1)
+
 s = [100, 2, 3, 45, -2, 4]
+
+
+from functools import reduce
+from operator import truediv
+
+def mean(numbers):
+    return truediv(*reduce(lambda a, b: (a[0] + b[1], b[0]),
+                           enumerate(numbers, start=1),
+                           (0, 0)))
+
 print(mean(s))
