f5fad2c4799b326da31e46965bda29b4112ad422
lolitiy <al.lolita96@gmail.com>
2019-11-24 13:50:41 +0300

    add script

--- 6.functions/6.6.py
+++ 6.functions/6.6.py
@@ -1,9 +1,19 @@
 #Мода
-def mode(arr) :
-    f = {}
-    for a in arr : f[a] = f.get(a,0)+1
-    m = max(f.values())
-    t = [(x,f[x]) for x in f if f[x]==m]
-    return m > 1 t[0][0] else None
+def mode(numbers):
+    largestCount = 0
+    modes = []
+    for x in numbers:
+        if x in modes:
+            continue
+        count = numbers.count(x)
+        if count > largestCount:
+            del modes[:]
+            modes.append(x)
+            largestCount = count
+        elif count == largestCount:
+            modes.append(x)
+    return modes
+
+
 s = [100, 3, 100, 2, 45, 45, 45, 3, 45, 3, 3, -2, 4]
 print(mode(s))
