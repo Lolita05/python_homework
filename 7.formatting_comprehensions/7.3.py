bfc0f8d14de1a8bec8f81bc3916364cadaf53d06
lolitiy <al.lolita96@gmail.com>
2019-11-24 14:41:36 +0300

    add script

--- 7.formatting_comprehensions/7.3.py
+++ 7.formatting_comprehensions/7.3.py
@@ -10,24 +10,9 @@ l = [[0, 1, 2, 3],[5, 6, 7, 8]]
 summa = [sum(i) for i in zip(*l)]
 
 #переходы из одного нуклеотида в другой (нуклеотиды должны быть разными) - 'A->T'
-sequence1 = ['A', 'T', 'G', 'C']
-sequence2 = ['A', 'T', 'G', 'C']
-sequence = [(f"{i} -> {j}") for i in sequence1 for j in sequence2 if i!=j]
+sequence = ['A', 'T', 'G', 'C']
+sequence = [(f"{i} -> {j}") for i in sequence for j in sequence if i!=j]
 print(sequence)
-
-def nucleotide_change(seq):
-    for n, i in enumerate(seq):
-        if i == 'A':
-            seq[n] = 'A -> T', 'A -> G', 'A -> C'
-        if i == 'T':
-            seq[n] = 'T -> A', 'T -> G', 'T -> C'
-        if i == 'G':
-            seq[n] = 'G -> A', 'G -> T', 'G -> C'
-        if i == 'C':
-            seq[n] = 'C -> A', 'C -> G', 'C -> A'
-    return seq
-
-print(nucleotide_change(sequence))
 
 #вложенные списки, представляющие матрицу 3 на 3, заполненную от 0 до 9
 matrix = [[j for j in range(i-2, i+1)] for i in range(3, 10, 3)]
