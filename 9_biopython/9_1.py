78789c12a2ac9eaa2589a3eb0660c446fcac68ea
lolitiy <al.lolita96@gmail.com>
2020-01-14 23:29:14 +0300

    add script

--- 9_biopython/9_1.py
+++ 9_biopython/9_1.py
@@ -1,13 +1,7 @@
 #fastq to fasta
-def fastq_to_fasta(fastq, fasta, min_length = 50):
-    i = 0
-    with open(fastq, "r") as fastq:
-        with open(fasta, "w") as fasta:
-            for line in fastq:
-                i += 1
-                if i % 4 == 1:
-                    title = ">" + line[1:len(line)]
-                if i % 4 == 2 and len(line) >= n:
-                    fasta.write(title)
-                    fasta.write(line)
+from Bio import SeqIO
 
+def fastq_to_fasta(fastq, fasta, min_length = 50):
+    fastq = SeqIO.parse(fastq, "fastq")
+    fastq = [record for record in fastq if len(record.seq) >= min_length]
+    SeqIO.write(fastq, fasta, "fasta")
