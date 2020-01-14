26f0afb0d01c76d37e14a033d93f39bca2fd2aa4
lolitiy <al.lolita96@gmail.com>
2020-01-14 23:04:25 +0300

    add script

--- 8.io_modules/8.4.py
+++ 8.io_modules/8.4.py
@@ -1,31 +1,23 @@
+#dfs
 
-adj_list = [[2, 4, 6],
-            [9],
-            [0, 3],
-            [2, 4],
-            [0, 3],
-            [],
-            [0, 7, 8],
-            [6],
-            [6],
-            [1]]
+def dfs(graph, node, visited):
+    if node not in visited:
+        visited.append(node)
+        for n in graph[node]:
+            dfs(graph,n, visited)
+    return visited
 
-def search(vertex, graph, visited):
-    visited[vertex] = True
+graph1 = {
+    'A' : ['B','S'],
+    'B' : ['A'],
+    'C' : ['D','E','F','S'],
+    'D' : ['C'],
+    'E' : ['C','H'],
+    'F' : ['C','G'],
+    'G' : ['F','S'],
+    'H' : ['E','G'],
+    'S' : ['A','C','G']
+}
 
-    for neighbour in graph[vertex]:
-        if not visited[neighbour]:
-            print(vertex, neighbour, visited)
-            search(neighbour, graph, visited)
-
-def dfs_num(graph):
-
-    visited = {v: False for v in graph}
-    a = 0
-    for v in graph:
-        if visited[v] == False:
-            a += 1
-        search(v, graph, visited)
-    return a
-
-print(dfs_num(adj_list))
+visited = dfs(graph1,'A', [])
+print(visited)
