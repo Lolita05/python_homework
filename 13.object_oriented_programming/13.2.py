b843ddb4723627c3deaf59a47535934381514471
lolitiy <al.lolita96@gmail.com>
2020-02-18 15:49:28 +0300

    add script

--- 13.object_oriented_programming/13.2.py
+++ 13.object_oriented_programming/13.2.py
@@ -20,24 +20,33 @@ class RNA:
         Function to translation of RNA seq into protein seq
         :return: protein seq
         """
-        seq = Seq(self.seq, IUPAC.unambiguous_rna)
-        tab = CodonTable.unambiguous_rna_by_name["Standard"]
-
-        if len(seq) % 3 == 0:
-            return seq.translate(table=tab)
-        elif (len(seq) + 1) % 3 == 0:
-            return seq.translate(table=tab) + Seq('N')
-        elif (len(seq) + 2) % 3 == 0:
-            return seq.translate(table=tab) + Seq('N') + Seq('N')
+        for i in self.seq:
+            if i not in "AUGC":
+                print("Invalid Input")
+                break
+            else:
+                seq = Seq(self.seq, IUPAC.unambiguous_rna)
+                tab = CodonTable.unambiguous_rna_by_name["Standard"]
+                if len(seq) % 3 == 0:
+                    return seq.translate(table=tab)
+                elif (len(seq) + 1) % 3 == 0:
+                    return seq.translate(table=tab) + Seq('N')
+                elif (len(seq) + 2) % 3 == 0:
+                    return seq.translate(table=tab) + Seq('N') + Seq('N')
 
     def reverse_transcription(self):
         """
         Function to convert rna seq to dna seq
         :return: dna seq
         """
-        dna = self.seq
-        dna = dna.translate(dna.maketrans("AUGC", "TACG"))
-        return dna
+        for i in self.seq:
+            if i not in "AUGC":
+                print("Invalid Input")
+                break
+            else:
+                dna = self.seq
+                dna = dna.translate(dna.maketrans("AUGC", "TACG"))
+                return dna
 
 
 a = RNA('AAAAUGAAGGGUGAAU')
