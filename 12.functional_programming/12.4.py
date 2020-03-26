90824cdff0f904f80f5b5c0da565629ba2c5754a
lolitiy <al.lolita96@gmail.com>
2020-03-26 16:11:06 +0300

    add script

--- 12.functional_programming/12.4.py
+++ 12.functional_programming/12.4.py
@@ -4,7 +4,8 @@ import itertools
 
 def generate(length):
     dna = ['A', 'G', 'C', 'T']
-    for item in itertools.product(dna, repeat = length):
-        yield "".join(item)
+    for seq in range(1, length + 1):
+        for item in itertools.product(dna, repeat = seq):
+            yield "".join(item)
 
 print(list(generate(2)))
