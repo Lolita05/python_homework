ef7ecb19e33d66eacc9fbaadbbbc083cf4c953b1
lolitiy <al.lolita96@gmail.com>
2020-03-29 21:53:58 +0300

    add script

--- 15/15.1.py
+++ 15/15.1.py
@@ -1,14 +1,11 @@
 import re
 
 
-f1 = open("/Users/lolitiy/Documents/inst_bioinf_19_20/python/references.txt", 'r')
+
 f2 = open("/Users/lolitiy/Documents/inst_bioinf_19_20/python/references2.txt", "w+")
 
 reg_pattern = r'/(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+/'
-
-for line in f1:
-    match = re.findall(reg_pattern, line)
-    for i in match:
-        f2.write(i + '\n')
-f2.close()
-f1.close()
+with open("/Users/lolitiy/Documents/inst_bioinf_19_20/python/references.txt", "r") as f1:
+    with open("/Users/lolitiy/Documents/inst_bioinf_19_20/python/references2.txt", "w+") as f2:
+        match = re.findall(reg_pattern, f1.read())
+        f2.write(‘\n’.join(match))
