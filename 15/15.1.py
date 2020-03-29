c170ddb1aabe735445156caecd66d47afc474c17
lolitiy <al.lolita96@gmail.com>
2020-03-29 21:54:24 +0300

    add script

--- 15/15.1.py
+++ 15/15.1.py
@@ -1,9 +1,6 @@
 import re
 
 
-
-f2 = open("/Users/lolitiy/Documents/inst_bioinf_19_20/python/references2.txt", "w+")
-
 reg_pattern = r'/(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+/'
 with open("/Users/lolitiy/Documents/inst_bioinf_19_20/python/references.txt", "r") as f1:
     with open("/Users/lolitiy/Documents/inst_bioinf_19_20/python/references2.txt", "w+") as f2:
